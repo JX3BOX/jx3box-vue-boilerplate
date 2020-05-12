3c4038c08f3da6c32afe1401332c52509a0336d1
iRuxu <rx6@qq.com>
2020-05-12 14:18:15 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -10,20 +10,6 @@ Vue.config.productionTip = false;
 import JX3BOX_UI from '@jx3box/jx3box-common-ui'
 Vue.use(JX3BOX_UI);
 
-// 扩展UI模块
-// import Comments from "@jx3box/jx3box-comment-ui"
-// Vue.use(Comments)
-
-// Axios实例
-const axios = require('axios');
-Vue.prototype.$axios = axios;
-Vue.prototype.$http = axios;
-// Vue.prototype.$axios.defaults.withCredentials = true;
-const _axios = axios.create({
-    withCredentials: true,
-})
-Vue.prototype.$https = _axios;
-
 // 数据与路由
 // import router from "../router";
 // import store from "../store";
