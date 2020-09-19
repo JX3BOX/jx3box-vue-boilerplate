27d06d80415414df950efed5c6bfffac474cba97
iRuxu <rx6@qq.com>
2020-09-19 20:35:35 +0800

    sync common

--- src/service/stat.js
+++ src/service/stat.js
@@ -3,9 +3,9 @@ import { __next } from "@jx3box/jx3box-common/js/jx3box.json";
 
 const stat = __next + "api/summary-any/";
 // const stat = "/api/summary-any/";
-function getStat(id) {
+function getStat(type,id) {
     return axios
-        .get(stat + 'wiki-' + id + "/stat")
+        .get(stat + type + '-' + id + "/stat")
         .then((res) => {
             return res.data;
         })
@@ -16,20 +16,20 @@ function getStat(id) {
 
 const actions = __next + "api/summary-any/";
 // const actions = "/api/summary-any/";
-function postStat(id) {
-    return axios.get(actions + 'wiki-' + id, {
+function postStat(type,id) {
+    return axios.get(actions + type + '-' + id, {
         params: {
-            type: "wiki",
+            type: type,
             actions: "views",
         },
     });
 }
 const rank = __next + 'api/summary/visit/rank'
 // const rank = "/api/summary/visit/rank";
-function getRank() {
+function getRank(type) {
     return axios.get(rank,{
         params : {
-            postType : 'wiki',
+            postType : type,
             postAction : 'views',
             pageSize : 10
         }
