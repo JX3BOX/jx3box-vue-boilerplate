2c958c7d5b440a67c700122c37e259d6ceb09689
iRuxu <rx6@qq.com>
2022-02-05 04:35:08 +0800

    perf:clean css

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
     <div id="app">
         <Header></Header>
-        <Breadcrumb
-            name="测试"
-            slug="test"
-            root="/test"
-            :publishEnable="true"
-            :adminEnable="true"
-            :feedbackEnable="true"
-            :crumbEnable="true"
-        >
+        <Breadcrumb name="测试" slug="test" root="/test" :publishEnable="true" :adminEnable="true" :feedbackEnable="true" :crumbEnable="true">
             <img slot="logo" svg-inline :src="getAppIcon('macro')" />
         </Breadcrumb>
         <LeftSidebar>left side</LeftSidebar>
         <Main :withoutRight="false">
+            <img src="@/assets/img/logo.svg" alt="">
             <div class="m-main">
                 <router-view />
             </div>
@@ -24,24 +17,19 @@
 </template>
 
 <script>
-import {getAppIcon} from '@jx3box/jx3box-common/js/utils'
+import { getAppIcon } from "@jx3box/jx3box-common/js/utils";
 export default {
     name: "App",
     props: [],
-    data: function () {
+    data: function() {
         return {};
     },
     computed: {},
-    methods: {getAppIcon},
+    methods: { getAppIcon },
     components: {},
 };
 </script>
 
 <style lang="less">
 @import "./assets/css/app.less";
-@media screen and (max-width: @phone) {
-    .c-crumb {
-        .none;
-    }
-}
 </style>
