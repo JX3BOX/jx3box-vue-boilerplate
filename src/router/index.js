20d936fb73ed720778921b14a0025c76a336978b
iRuxu <rx6@qq.com>
2022-02-11 19:32:59 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+Vue.use(VueRouter);
 
 const Index = () => import("../views/Index.vue");
-// const Fn = () => import("../views/Fn.vue");
-
-Vue.use(VueRouter);
 
 const routes = [
     { name: "index", path: "/", component: Index },
-    // { name: "fn", path: "/fn", component: Fn },
 ];
 
 const router = new VueRouter({
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,12 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
 Vue.use(Vuex);
 
 let store = {
     state: {
         client: location.href.includes("origin") ? "origin" : "std",
     },
-    mutations: {
-        switchClient: function(state, val) {
-            state.client = val || "std";
-        },
-    },
+    mutations: {},
     getters: {},
     actions: {},
     modules: {},
